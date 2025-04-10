e13e83470fcdafb71406c9466f19d437c30c42b6
22amiibo <113854467+22amiibo@users.noreply.github.com>
2025-04-10 14:37:09 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "YAIzaSyD1Mg3AnBfXyA6po0xLOJmf2VhIU999i_0",
+  authDomain: "psych-edac2.firebaseapp.com",
+  projectId: "psych-edac2",
+  storageBucket: "psych-edac2.firebasestorage.app",
+  messagingSenderId: "947042091001",
+  appId: "1:947042091001:web:909fa00905f976e0814728"
 };
 
 // Initialize Firebase
